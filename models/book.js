cf00ad1201ab187e2ea6387de021ae3c97d5052a
kaylawilson535 <42094645+kaylawilson535@users.noreply.github.com>
2020-11-04 09:03:19 -0700

    File Upload Database

--- models/book.js
+++ models/book.js
@@ -1,8 +1,6 @@
 //typically name it a singular form of the route
 
 const mongoose = require('mongoose')
-const path = require('path')
-const coverImageBasePath = 'uploads/bookCovers'
 
 const bookSchema = new mongoose.Schema({
     title: {
@@ -25,10 +23,14 @@ const bookSchema = new mongoose.Schema({
         required: true,
         default: Date.now
     },
-    coverImageName: {
+    coverImage: {
+        type: Buffer,
+        required: true
+      },
+      coverImageType: {
         type: String,
         required: true
-    },
+      },
     author: {
         type: mongoose.Schema.Types.ObjectId, //this references another object inside of the collections
         required: true,
@@ -36,11 +38,13 @@ const bookSchema = new mongoose.Schema({
     }
 })
 
-bookSchema.virtual('coverImagePath').get(function () {
-    if(this.coverImageName != null){
-        return path.join('/', coverImageBasePath, this.coverImageName)
+//this somehow explains my yt comment. i think its creating
+//another property called coverImagePath that uses whats
+//already there
+bookSchema.virtual('coverImagePath').get(function() {
+    if (this.coverImage != null && this.coverImageType != null) {
+      return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
     }
-})
+  })
 
 module.exports = mongoose.model('Book', bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
